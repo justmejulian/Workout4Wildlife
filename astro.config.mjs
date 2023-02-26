a914b7e3c2c0877017eccaec3276aac21a48859f
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2023-02-26 14:09:16 +0100

    deploy to github. closes #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
 export default defineConfig({
+  site: 'https://justmejulian.github.io',
+  base: '/Workout4Wildlife',
   integrations: [react()],
 })
