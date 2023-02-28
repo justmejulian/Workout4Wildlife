421e794716d906067b75bb4885b6227028ea90b9
Moritz Bättig <moritz.baettig@bluewin.ch>
2023-02-28 13:42:00 +0100

    Prettier

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,18 +2,13 @@ import type { MouseEventHandler } from 'react';
 import styles from './Button.module.css';
 
 interface ButtonProps {
-  title: string,
-  onClick: MouseEventHandler<HTMLButtonElement>
+  title: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
 function Button(props: ButtonProps) {
-
   return (
-    <button
-      type="button"
-      className={styles.button}
-      onClick={props.onClick}
-    >
+    <button type="button" className={styles.button} onClick={props.onClick}>
       {props.title}
     </button>
   );
