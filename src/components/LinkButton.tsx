1e75367d7064dc13de9135faac895373a907c7ae
Moritz Bättig <moritz.baettig@bluewin.ch>
2023-02-28 13:33:10 +0100

    Added LinkButton component

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,19 @@
+import Button from '../components/Button';
+
+interface LinkButtonProps {
+    title: string,
+    url: string
+  }
+
+function LinkButton(props: LinkButtonProps) {
+    return (
+        <Button
+            onClick={() => {
+                window.open(props.url, '_blank')?.focus();
+            }}
+            title={props.title}
+        />
+    );
+}
+
+export default LinkButton;
