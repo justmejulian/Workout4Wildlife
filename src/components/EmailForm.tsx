9795e60bd543964dcee74413708496f8165c6160
Moritz Bättig <moritz.baettig@bluewin.ch>
2023-03-09 22:46:28 +0100

    Created EmailForm component

---
+++ src/components/EmailForm.tsx
@@ -0,0 +1,28 @@
+import type { FormEvent } from 'react';
+import styles from './EmailForm.module.css';
+
+function EmailForm() {
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form as HTMLFormElement);
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
+  return (
+    <form className={styles.form} method="post" onSubmit={handleSubmit}>
+      <input
+        name="email"
+        type="text"
+        placeholder="Enter your email for Viatu Newsletter"
+        className={styles.input}
+      />
+      <button type="submit" className={styles.submit}>
+        Subscribe Now
+      </button>
+    </form>
+  );
+}
+
+export default EmailForm;
