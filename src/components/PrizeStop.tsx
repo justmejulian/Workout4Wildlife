89b7a04a93b75fa79398ee7baba4ad37bd52abc5
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2023-03-24 16:37:44 +0100

    added minimal swipe

--- src/components/PrizeStop.tsx
+++ src/components/PrizeStop.tsx
@@ -1,3 +1,4 @@
+import cls from 'classnames';
 import styles from './PrizeStop.module.css';
 
 export interface Stop {
@@ -13,10 +14,18 @@ interface PrizeStopProps {
 
 function PrizeStop({ stop }: PrizeStopProps) {
   return (
-    <div className={styles.stop}>
-      <img className={styles.cover} src={stop.cover} alt={stop.title + ' cover'} />
+    <div className={cls(styles.stop, styles.animate)}>
+      <img
+        className={styles.cover}
+        src={stop.cover}
+        alt={stop.title + ' cover'}
+      />
       <div className={styles.description}>
-        <img className={styles.logo} src={stop.logo} alt={stop.title + ' logo'} />
+        <img
+          className={styles.logo}
+          src={stop.logo}
+          alt={stop.title + ' logo'}
+        />
         <p className={styles.title}>{stop.title}</p>
         <p>{stop.text}</p>
       </div>
--- src/components/PrizeStops.tsx
+++ src/components/PrizeStops.tsx
@@ -1,4 +1,4 @@
-import cls from 'classnames'
+import cls from 'classnames';
 import styles from './PrizeStops.module.css';
 
 import PrizeStop, { Stop } from './PrizeStop';
@@ -9,24 +9,61 @@ interface PrizeStopsProps {
 }
 
 function PrizeStops({ stops }: PrizeStopsProps) {
-  const [selected, setSelected] = useState(1)
+  const [selected, setSelected] = useState(1);
+  const [touchPosition, setTouchPosition] = useState(null);
+
+  const handleTouchStart = (e) => {
+    const touchDown = e.touches[0].clientX;
+    setTouchPosition(touchDown);
+  };
+
+  const handleTouchMove = (e) => {
+    const touchDown = touchPosition;
+
+    if (touchDown === null) {
+      return;
+    }
+
+    const currentTouch = e.touches[0].clientX;
+    const diff = touchDown - currentTouch;
+
+    if (diff > 5) {
+      setSelected((prevState) =>
+        prevState >= stops.length - 1 ? 0 : prevState + 1
+      );
+    }
+
+    if (diff < -5) {
+      setSelected((prevState) =>
+        prevState <= 0 ? stops.length - 1 : prevState - 1
+      );
+    }
+
+    setTouchPosition(null);
+  };
 
   return (
     <div className={styles.container}>
-      {stops[selected] &&
-        <PrizeStop key={stops[selected]?.title} stop={stops[selected] as Stop} />
-      }
-      <div className={styles.dotContainer}>
-        {
-          stops.map((stop, index) => (
-            <div
-              key={'dot' + stop.title}
-              className={cls(styles.dot, { [styles.selected as string]: selected === index })}
-              onClick={() => setSelected(index)} ></div>
-          ))
-        }
+      <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
+        {stops[selected] && (
+          <PrizeStop
+            key={stops[selected]?.title}
+            stop={stops[selected] as Stop}
+          />
+        )}
       </div>
-    </div >
+      <div className={styles.dotContainer}>
+        {stops.map((stop, index) => (
+          <div
+            key={'dot' + stop.title}
+            className={cls(styles.dot, {
+              [styles.selected as string]: selected === index,
+            })}
+            onClick={() => setSelected(index)}
+          ></div>
+        ))}
+      </div>
+    </div>
   );
 }
 
