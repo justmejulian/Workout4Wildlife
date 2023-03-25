e48471978b3e8fd256fcc0275dd9208fbde626fb
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2023-03-25 16:52:19 +0100

    using swiperjs instead of my own mess

--- src/components/PrizeStops.tsx
+++ src/components/PrizeStops.tsx
@@ -1,68 +1,33 @@
-import cls from 'classnames';
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 import styles from './PrizeStops.module.css';
 
 import PrizeStop, { Stop } from './PrizeStop';
-import { useState } from 'react';
 
 interface PrizeStopsProps {
   stops: Stop[];
 }
 
 function PrizeStops({ stops }: PrizeStopsProps) {
-  const [selected, setSelected] = useState(1);
-  const [touchPosition, setTouchPosition] = useState(null);
-
-  const handleTouchStart = (e) => {
-    const touchDown = e.touches[0].clientX;
-    setTouchPosition(touchDown);
-  };
-
-  const handleTouchMove = (e) => {
-    const touchDown = touchPosition;
-
-    if (touchDown === null) {
-      return;
-    }
-
-    const currentTouch = e.touches[0].clientX;
-    const diff = touchDown - currentTouch;
-
-    if (diff > 5) {
-      setSelected((prevState) =>
-        prevState >= stops.length - 1 ? 0 : prevState + 1
-      );
-    }
-
-    if (diff < -5) {
-      setSelected((prevState) =>
-        prevState <= 0 ? stops.length - 1 : prevState - 1
-      );
-    }
-
-    setTouchPosition(null);
-  };
-
   return (
     <div className={styles.container}>
-      <div onTouchStart={handleTouchStart} onTouchMove={handleTouchMove}>
-        {stops[selected] && (
-          <PrizeStop
-            key={stops[selected]?.title}
-            stop={stops[selected] as Stop}
-          />
-        )}
-      </div>
-      <div className={styles.dotContainer}>
-        {stops.map((stop, index) => (
-          <div
-            key={'dot' + stop.title}
-            className={cls(styles.dot, {
-              [styles.selected as string]: selected === index,
-            })}
-            onClick={() => setSelected(index)}
-          ></div>
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        pagination
+        loop
+      >
+        {stops.map((stop) => (
+          <SwiperSlide key={stop?.title}>
+            <PrizeStop stop={stop} />
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
     </div>
   );
 }
